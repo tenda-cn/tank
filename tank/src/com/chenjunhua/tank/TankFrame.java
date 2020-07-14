5c3e6820cfecac5777b8cc3337a9842466b654cf
Gogs <gogs@fake.local>
2020-07-14 10:29:50 +0800

    添加键盘监听事件

--- tank/src/com/chenjunhua/tank/TankFrame.java
+++ tank/src/com/chenjunhua/tank/TankFrame.java
@@ -1,18 +1,41 @@
 package com.chenjunhua.tank;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TankFrame extends Frame {
+	int x=100,y=100;
+	
+	
 	public TankFrame() {
 		setSize(800,600);
 		setResizable(false);
 		setTitle("tank war");
 		setVisible(true);
+	
+		this.addKeyListener(new MyKeyListener());
+		
 	}
 	@Override
 	public void paint(Graphics g) {
-		g.fill3DRect(200, 200, 50, 50, true);
+		System.out.println("paint");
+		g.fill3DRect(x, y, 50, 50, true);
+		x+=10;
+		y+=10;
 	}
 	
+	class MyKeyListener extends KeyAdapter{
 
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			System.out.println("Key pressed");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			System.out.println("Key release");
+		}
+		
+	}
 }
