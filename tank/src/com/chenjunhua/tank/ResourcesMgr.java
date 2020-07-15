52eec732ca60d79c7b05c6e0c86d779891a92eaf
Gogs <gogs@fake.local>
2020-07-15 17:03:04 +0800

    new出5个敌人坦克

--- tank/src/com/chenjunhua/tank/ResourcesMgr.java
+++ tank/src/com/chenjunhua/tank/ResourcesMgr.java
@@ -7,12 +7,17 @@ import javax.imageio.ImageIO;
 
 public class ResourcesMgr {
 	public static BufferedImage tankL, tankU, tankR, tankD;
+	public static BufferedImage bulletL, bulletU, bulletR, bulletD;
 	static {
 		try {
 			tankL = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
 			tankU = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
 			tankR = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
 			tankD = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+			bulletD = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+			bulletL = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+			bulletR = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+			bulletU = ImageIO.read(ResourcesMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
 		
 		
 		} catch (IOException e) {
