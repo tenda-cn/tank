f04705ae26fade1cb692ccb61b38c063e4f538bd
Gogs <gogs@fake.local>
2020-07-15 16:39:23 +0800

    子弹换成图片

--- tank/src/com/chenjunhua/tank/Bullet.java
+++ tank/src/com/chenjunhua/tank/Bullet.java
@@ -21,10 +21,24 @@ public class Bullet {
 	public void paint(Graphics g) {
 		if(!live)
 			tf.bullets.remove(this);
-		Color color = g.getColor();
-		g.setColor(Color.red);
-		g.fillOval(x, y, WIDTH, HEIGHT);
-		g.setColor(color);
+		switch (dir) {
+		case LEFT:
+			g.drawImage(ResourcesMgr.bulletL, x, y, null);
+			break;
+		case RIGHT:
+			g.drawImage(ResourcesMgr.bulletR, x, y, null);
+			break;
+		case UP:
+			g.drawImage(ResourcesMgr.bulletU, x, y, null);
+			break;
+		case DOWN:
+			g.drawImage(ResourcesMgr.bulletD, x, y, null);
+			break;
+
+		default:
+			break;
+		}
+		
 		move();
 	}
 
